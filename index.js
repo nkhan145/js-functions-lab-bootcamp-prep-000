b059f083df1524c70bfead66a2cb8b7972adf522
Nehal Khan <nehal97@hotmail.co.uk>
2019-06-25 16:30:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// write your code below!
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(Khan) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(Easter, Khan) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown("7","" Easter") {
+  return `It's ${days} days until ${holiday}!`
+}
+ 
